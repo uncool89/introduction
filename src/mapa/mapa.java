55915a818684771a420d508611bcc10a18243dc7
Kamil <kjez1989@gmail.com>
2016-05-05 23:14:07 +0200

    Initial commit

---
+++ src/mapa/mapa.java
@@ -0,0 +1,29 @@
+package mapa;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class mapa {
+
+	public static void main(String[] args) {
+	Map<String, String> map = new HashMap<String, String>();
+	map.put("1", "Jan");
+	map.put("2", "Feb");
+	map.put("3", "Mar");
+	
+	//loop a Map
+	for (Map.Entry<String, String> entry : map.entrySet()) {
+		System.out.println("Key : " + entry.getKey() + " Value : " + entry.getValue());
+	}
+	
+	String value = map.get("1").toLowerCase();
+	System.out.print(value);
+	String value1 = map.get("3").toLowerCase();
+	System.out.print(value1);
+
+    System.out.println("Key set values are: " + map.keySet());
+	
+	
+	}
+}
