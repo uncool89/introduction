847faefc615adcace4e0d13287213118cffeb5e0
Kamil <kjez1989@gmail.com>
2016-05-09 01:58:23 +0200

    Minor changes in mapa

--- src/mapa/mapa.java
+++ src/mapa/mapa.java
@@ -1,34 +1,10 @@
-package mapa;
-
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.List;
 
 public class mapa {
 
 	public static void main(String[] args) {
-    String test = "test";
-	Map<String, String> map = new HashMap<String, String>();
-	map.put("1", "Audi");
-	map.put("2", "Mercedes");
-	map.put("3", "BMW");
-	
-	//loop a Map
-	for (Map.Entry<String, String> entry : map.entrySet()) {
-		System.out.println("Key : " + entry.getKey() + " Value : " + entry.getValue());
+	List <String> testList = new ArrayList<>();
+	testList.forEach(System.out::println);
+    Lambda.execute();
 	}
-	
-	String value = map.get("1").toLowerCase();
-	System.out.print(value);
-	String value1 = map.get("3").toLowerCase();
-	System.out.print(value1);
-
-    System.out.println("Key set values are: " + map.keySet());
-	
-	
-	}
-	
-	private static void doSomething(){
-		System.out.println.("Do something");
-	}
-}
