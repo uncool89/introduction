150415909fe21c7bf3e39c158966a79708ab4a75
Kamil <kjez1989@gmail.com>
2016-05-09 02:46:16 +0200

    Last modification from local

--- src/mapa/mapa.java
+++ src/mapa/mapa.java
@@ -7,8 +7,8 @@ public class mapa {
 
 	public static void main(String[] args) {
 	List <String> testList = new ArrayList<>();
-	testList.add("MAZDA");
-	testList.add("OPEL");
+	testList.add("TESLA");
+	testList.add("JAGUAR");
 	testList.forEach((x)->System.out.println(x));
 	}
 }
