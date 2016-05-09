3b888e31c7284c855e4d032f45b5457f03847eb4
uncool89 <kjez1989@gmail.com>
2016-05-09 02:29:15 +0200

    Minor update in Lambda class from remote

--- src/mapa/Lambda.java
+++ src/mapa/Lambda.java
@@ -3,7 +3,7 @@ package mapa;
 public class Lambda {
 	
 	public static void execute() {
-		repeat(10, () -> System.out.print("Wow!!!"));
+		repeat(10, () -> System.out.print("This is change made from master!!!"));
 	}
 
 	public static void repeat(int n, IntConsumer action) {
