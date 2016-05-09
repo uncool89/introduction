2418ea0164fb98986542b5db8bd122d508a3401a
Kamil <kjez1989@gmail.com>
2016-05-09 02:28:15 +0200

    Changed content of Lambda class

--- src/mapa/Lambda.java
+++ src/mapa/Lambda.java
@@ -3,11 +3,11 @@ package mapa;
 public class Lambda {
 	
 	public static void execute() {
-		repeat(10, () -> System.out.print("This is change made from master!!!"));
+		repeat(10, () -> System.out.print("Change this!!!"));
 	}
 
-	public static void repeat(int n, IntConsumer action) {
-		for(int i = 0; i<n; i++) action.accept(i);
+	public static void repeat(int n, Runnable action) {
+		for(int i = 0; i<n; i++) action.run();
 
 	}
 }
