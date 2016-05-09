3fce455760871dc662708040a39c391f1ae7631e
Kamil <kjez1989@gmail.com>
2016-05-09 08:31:03 +0200

    Changed content of  map locally

--- src/mapa/mapa.java
+++ src/mapa/mapa.java
@@ -9,7 +9,7 @@ public class mapa {
         final String test = "Samochody";
 	List <String> testList = new ArrayList<>();
 	testList.add("HONDA");
-	testList.add("JAGUAR");
+	testList.add("TOYOTA");
 	testList.forEach((x)->System.out.println(x));
 	Lambda.execute();
 	}
