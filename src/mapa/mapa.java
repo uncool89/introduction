d7e5ecee7906faea60f09407191766a998987749
Michal Zajac <michal.zajac@gft.com>
2016-05-13 10:40:36 +0200

    kota nie ma w domu

--- src/mapa/mapa.java
+++ src/mapa/mapa.java
@@ -9,7 +9,7 @@ public class mapa {
 
 	public static void main(String[] args) {
 	List <String> testList = new ArrayList<>();
-	testList.add("HONDA");
+	testList.add("HONDA mamamammamamamma mamamammamamma brum brum brum");
 	testList.add("TOYOTA");
 	testList.forEach((x)->System.out.println(x));
 
