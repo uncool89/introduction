ec54a818cf0142920612ae3be260d42c60d233d0
slwk <sylwester.balcerek@gmail.com>
2016-05-13 10:29:18 +0200

    modified Lambda

--- src/mapa/Lambda.java
+++ src/mapa/Lambda.java
@@ -1,13 +1,17 @@
 package mapa;
 
+import java.util.stream.IntStream;
+
 public class Lambda {
 	
 	public static void execute() {
-		repeat(10, () -> System.out.print("Change this!!!"));
+		IntStream.range(0,10).forEach(value -> System.out.println("Do it: "+value));
 	}
 
 	public static void repeat(int n, Runnable action) {
 		for(int i = 0; i<n; i++) action.run();
 
 	}
+
+
 }
