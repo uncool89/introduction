e8f42279321f2fd579e10d29bafbe3d488022f29
Barbara Pietras <barbarapietrass@gmail.com>
2016-05-13 10:29:04 +0200

    Some kind of Basia comment

--- src/mapa/mapa.java
+++ src/mapa/mapa.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class mapa {
-
+             //czemu ta klasa tak sie nazywa?
 	public static void main(String[] args) {
 	List <String> testList = new ArrayList<>();
 	testList.add("HONDA");
