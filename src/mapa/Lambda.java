18e180fbbd84b42863159d2c965cd825a95410ff
slwk <sylwester.balcerek@gmail.com>
2016-05-13 10:40:03 +0200

    Modidfied lambda for MZ

--- src/mapa/Lambda.java
+++ src/mapa/Lambda.java
@@ -3,7 +3,7 @@ package mapa;
 public class Lambda {
 	
 	public static void execute() {
-		repeat(10, () -> System.out.print("Change this!!!"));
+		repeat(10, () -> System.out.print("Zajac je marchewke"));
 	}
 
 	public static void repeat(int n, Runnable action) {
