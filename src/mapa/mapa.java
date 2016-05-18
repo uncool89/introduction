112abbc4f465dc5461569b82dc12ca49cd575412
uncool89 <kjez1989@gmail.com>
2016-05-18 07:43:27 +0200

    this is another change

--- src/mapa/mapa.java
+++ src/mapa/mapa.java
@@ -7,8 +7,7 @@ public class mapa {
 
 	public static void main(String[] args) {
 	List <String> testList = new ArrayList<>();
-	testList.add("HONDA suxxxx :)");
-	testList.add("TOYOTA");
+	testList.add("OPEL");
 	testList.forEach((x)->System.out.println(x));
 	Lambda.execute();
 	}
